d001d48922f5d08bba18e84bf9c944b11110c6c4
Sergey K. <mock@email.zxc>
2023-12-02 00:05:49 +0300

    Добавлена mongo

--- src/controllers/cards.controller.js
+++ src/controllers/cards.controller.js
@@ -1,22 +1,47 @@
-const { extendedData } = require ("../constants");
+const Card = require('../models/card');
 
-function getCards(req, res) {
-  res.status(200).send({
-    data: extendedData,
-  });
+async function getCards(req, res) {
+  try {
+    const cards = await Card.find(); // Получаем все карточки из базы данных
+    res.status(200).send({
+      data: cards,
+    });
+  } catch (err) {
+    res.status(500).send({
+      error: 'Failed to fetch cards from the database',
+    });
+  }
 }
 
-function getCard(req, res) {
+async function getCard(req, res) {
   const { id } = req.params;
-  res.status(200).send(extendedData[id - 1]);
+  try {
+    const card = await Card.findById(id); // Находим карточку по ID в базе данных
+    if (!card) {
+      return res.status(404).send({
+        error: 'Card not found',
+      });
+    }
+    res.status(200).send(card);
+  } catch (err) {
+    res.status(500).send({
+      error: 'Failed to fetch card from the database',
+    });
+  }
 }
 
-function addCard(req, res) {
+async function addCard(req, res) {
   const fullCard = req.body;
 
-  const card = {...fullCard, id: extendedData.length + 1};
-  extendedData.push(card);
-  res.status(200).send(card);
+  try {
+    const newCard = new Card(fullCard); // Создаем новую карточку на основе данных из запроса
+    await newCard.save(); // Сохраняем новую карточку в базе данных
+    res.status(200).send(newCard);
+  } catch (err) {
+    res.status(500).send({
+      error: 'Failed to add card to the database',
+    });
+  }
 }
 
 module.exports = {
--- src/index.js
+++ src/index.js
@@ -2,14 +2,20 @@ const express = require("express");
 const bodyParser = require('body-parser')
 const { cardsRouter } = require('./routes/cards.js');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 const PORT = 5000;
+const DB_URL = 'mongodb://127.0.0.1:27017/characters';
 
 const app = express();
 
 app.use(cors())
 app.use(bodyParser.json())
 app.use('/api', cardsRouter)
+
+mongoose.connect(DB_URL, {
+  useNewUrlParser: true,
+})
 
 app.listen(PORT, () => {
   console.log(`Server started at ${PORT}`);
